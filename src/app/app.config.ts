19ecf3bfb6b908d2c20f52390508f00304360143
ousmane sow <thifuge@gmail.com>
2026-06-11 16:22:05 +0200

    feat(config): add customised preset for primary theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,26 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
+
+const definedPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{red.800}',
+      100: '{red.800}',
+      200: '{red.800}',
+      300: '{red.800}',
+      400: '{red.800}',
+      500: '{red.800}',
+      600: '{red.800}',
+      700: '{red.800}',
+      800: '{red.800}',
+      900: '{red.900}',
+      950: '{red.950}'
+    }
+  }
+})
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +31,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
      providePrimeNG({
             theme: {
-                preset: Aura,
+                preset: definedPreset,
                 options: {
                   darkModeSelector: '.app-dark'
                 }
