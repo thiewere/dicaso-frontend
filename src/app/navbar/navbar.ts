b28f2d9ae122392e94d7570adee8e6e7d2877208
ousmane sow <thifuge@gmail.com>
2026-06-11 15:06:13 +0200

    feat(nav): implement the navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
