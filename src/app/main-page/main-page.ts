5857d6dcd3542fbf65ff7ae7bc1c55493ff2b2ed
ousmane sow <thifuge@gmail.com>
2026-06-11 17:40:17 +0200

    feat(main): design main-page

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from "primeng/button";
+
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
