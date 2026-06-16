f65a81809badeb0952699dafaaa8aada7b23467d
ousmane sow <thifuge@gmail.com>
2026-06-16 21:24:59 +0200

    fix(nav): fix missing imports for the logo link

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule],
+  imports: [ButtonModule, RouterModule, NgOptimizedImage],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
